a934c7523baa066616c2f34811fdf2bd35689d7b
adinfinitum225 <zach.13.wells@gmail.com>
2019-12-17 21:48:54 -0600

    Fix 403 and 404 errors on QC.net

--- funnel/feeds.py
+++ funnel/feeds.py
@@ -1,4 +1,5 @@
-from urllib.request import urlopen, urljoin
+import re
+from urllib.request import urlopen, urljoin, Request
 from urllib.parse import urlsplit
 from xml.etree.ElementTree import parse
 
@@ -30,11 +31,14 @@ def index():
 
 @bp.route('/subscribe', methods=('POST',))
 def subscribe():
+    #add error handling for http exceptions
     url = request.form['url']
     db = get_db()
     cur = db.cursor()
 
-    xml_response = urlopen(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'} 
+    req = Request(url=url, headers=headers)
+    xml_response = urlopen(req)
 
     cur.execute(
             'INSERT INTO feeds (username, url) VALUES (%s, %s)',
@@ -46,7 +50,9 @@ def subscribe():
 
 def digest_feeds(urls):
     def filter_feed(url):
-        with urlopen(url) as response:
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'} 
+        req = Request(url=url, headers=headers)
+        with urlopen(req) as response:
             tree = parse(response)
         root = tree.getroot()
         if root.tag == 'rss':
@@ -55,6 +61,7 @@ def digest_feeds(urls):
             return aparse.AtomFeed.filter_feed(tree)
 
     feeds = []
+    
     for url in urls:
         url = normalize_url(url[0])
         feed = filter_feed(url)
@@ -62,8 +69,13 @@ def digest_feeds(urls):
     return feeds
 
     
+#Make a separate norm for comparing urls vs subscribing?
 def normalize_url(url):
-    url = url.casefold()
+    resource = re.search(r'/[^/]+$', url)
+    end = resource.group()
+    address = url[0:resource.start()]
+    address = address.casefold()
+    url = address + end
     url = url.strip()
     url = url.split('www.')
     url = url[-1].split('//')
