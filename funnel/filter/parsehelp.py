b6eaf2ba1f269b63f0e092dc24d4213be824ee84
adinfinitum225 <zach.13.wells@gmail.com>
2019-12-21 17:12:10 -0600

    Added support for rss pubdate with full month name. Closes #13

--- funnel/filter/parsehelp.py
+++ funnel/filter/parsehelp.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 class ParseHelp:
     def to_pydate(date):
-        months = dict(Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6, 
-                      Jul=7, Aug=8, Sep=9, Oct=10, Nov=11, Dec=12)
-        match = re.search(r'\d\d+ [a-zA-Z]{3} (19|20)\d\d (\d\d(.|$))*', date)
+        months = dict(Jan=1, January=1, Feb=2, February=2, Mar=3, March=3, Apr=4, April=4, 
+                      May=5, Jun=6, June=6, Jul=7, July=7, Aug=8, August=8, Sep=9, September=9,
+                      Oct=10, October=10, Nov=11, November=11, Dec=12, December=12)
+        match = re.search(r'\d\d+ [a-zA-Z]+ (19|20)\d\d (\d\d(.|$))*', date)
         if match:
             pieces = match.group().split()
             pieces.extend(pieces.pop(-1).split(':'))
